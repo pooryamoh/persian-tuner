0b277eaca7fb8e3d6d2808c14febf3cede530106
pooryamoh <44756120+pooryamoh@users.noreply.github.com>
2025-03-29 16:51:30 +0330

    fix bugs due to merge

--- src/App.tsx
+++ src/App.tsx
@@ -125,7 +125,6 @@ export default function App() {
         height: "100vh",
       }}
     >
-<<<<<<< HEAD
       {!start && (
         <button
           className="rounded-md bg-blue-600 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
@@ -136,17 +135,6 @@ export default function App() {
         </button>
       )}
       {pitch && <div style={{ fontSize: "2rem" }}>{pitch.toFixed(2)} Hz</div>}
-=======
-      <button
-        css={{
-          color: "hotpink",
-        }}
-        onClick={startAnalyser}
-      >
-        start
-      </button>
-      {pitch && `${pitch} Hz`}
->>>>>>> d42491af3a2195863306ee55b7296590c7ac8d1c
       {pitch && (
         <div style={{ display: "flex", gap: "0.5rem" }}>
           <div css={noteAroundAroundStyle}>
