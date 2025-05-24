1191e7988e12468b4d3f964b96f5e7fe89747f2b
pooryamoh <44756120+pooryamoh@users.noreply.github.com>
2025-05-24 18:57:13 +0330

    fix bemol

--- src/components/Bemol.tsx
+++ src/components/Bemol.tsx
@@ -1,4 +1,4 @@
 import { memo } from "react";
 
-const Bemol = memo(() => <span className="icon-Bemol"></span>);
+const Bemol = memo(() => <span className="icon-Flat"></span>);
 export default Bemol;
